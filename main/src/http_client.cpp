3f66c65cb46f20ffbd04d93546fdd597fa3991af
Abhishek-0809 <abhishekwaghmare34@gmail.com>
2024-07-18 13:41:30 +0530

    Optimised the existing code

--- main/src/http_client.cpp
+++ main/src/http_client.cpp
@@ -17,60 +17,62 @@ uint8_t validTokenFlag = 0;
 static esp_err_t http_event_handler(esp_http_client_event_t *evt)
 {
     switch (evt->event_id)
+    {
+    case HTTP_EVENT_ERROR:
+        ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+        break;
+
+    case HTTP_EVENT_REDIRECT:
+        ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
+        break;
+
+    case HTTP_EVENT_ON_CONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+        break;
+
+    case HTTP_EVENT_HEADER_SENT:
+        ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+        break;
+
+    case HTTP_EVENT_ON_HEADER:
+        if (tokenRequestFlag && !validTokenFlag)
         {
-        case HTTP_EVENT_ERROR:
-            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
-            break;
-
-        case HTTP_EVENT_REDIRECT:
-            ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
-            break;
-
-        case HTTP_EVENT_ON_CONNECTED:
-            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
-            break;
-
-        case HTTP_EVENT_HEADER_SENT:
-            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
-            break;
-
-        case HTTP_EVENT_ON_HEADER:
-            // ESP_LOGI("LOG", "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
-            // ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
-            if (tokenRequestFlag && (validTokenFlag == 0))
+            if (memcmp(evt->header_key, "x-cubbies-box-token", 19) == 0)
             {
-                if (memcmp(evt->header_key, "x-cubbies-box-token", 19) == 0) // First 19 bytes are the same
+                if (strlen(evt->header_value) > 40)
                 {
-                    if (strlen(evt->header_value) > 50)
-                    {
-                        ESP_LOGI("Error", "tokenLen=%d, too long. Max limit = 1kb", strlen(evt->header_value));
-                        break;
-                    }
-                    ESP_LOGI("\n HeaderFound", "\n key=%s, \n value=%s, \n keyLen=%d, \n tokenLen=%d", evt->header_key, evt->header_value, strlen(evt->header_key), strlen(evt->header_value));
-                    memcpy(accessToken, evt->header_value, strlen(evt->header_value));
-
-                    ESP_LOGI("\n accessToken", "value=%s", evt->header_value);
-                    validTokenFlag = 1;
+                    ESP_LOGI("Error", "Token too long. Max limit = 40 characters");
+                    break;
                 }
+                ESP_LOGI("HeaderFound", "key=%s, value=%s", evt->header_key, evt->header_value);
+                strncpy(accessToken, evt->header_value, sizeof(accessToken) - 1);
+                accessToken[sizeof(accessToken) - 1] = '\0';
+                ESP_LOGI("accessToken", "value=%s", accessToken);
+                validTokenFlag = 1;
             }
-            break;
+        }
+        break;
 
-        case HTTP_EVENT_ON_DATA:
-            // ESP_LOGW(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-            // ESP_LOGW("L", "l=%d", evt->data_len);
-            memcpy(outputBuff, evt->data, evt->data_len);
-            // ESP_LOGW(TAG, "%s", outputBuff);
-            outputBuff[evt->data_len] = '\0';
-            ESP_LOGI(TAG, "Response data NEW: %s", outputBuff);
-            break;
-        case HTTP_EVENT_ON_FINISH:
-            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
-            break;
-        case HTTP_EVENT_DISCONNECTED:
-            ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+    case HTTP_EVENT_ON_DATA:
+        if (evt->data_len > sizeof(outputBuff) - 1)
+        {
+            ESP_LOGE(TAG, "Received data length exceeds buffer size");
             break;
         }
-        return ESP_OK;
+        memcpy(outputBuff, evt->data, evt->data_len);
+        outputBuff[evt->data_len] = '\0';
+        ESP_LOGI(TAG, "Response Data: %s", outputBuff);
+        break;
+
+    case HTTP_EVENT_ON_FINISH:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+        break;
+
+    case HTTP_EVENT_DISCONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+        break;
+    }
+    return ESP_OK;
 }
 
 void http_post_task(void *pvParameters)
@@ -86,9 +88,6 @@ void http_post_task(void *pvParameters)
 
     const char *post_data = "{\"macAddress\":\"08:3A:F2:31:2D:8C\",\"boxUniqueId\":\"258ebc0f-2de5-471d-9d83-716cce092625\"}";
 
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-
-    esp_http_client_set_url(client, POST_URL);
     esp_http_client_set_method(client, HTTP_METHOD_POST);
     esp_http_client_set_header(client, "Content-Type", "application/json");
     esp_http_client_set_post_field(client, post_data, strlen(post_data));
@@ -99,16 +98,6 @@ void http_post_task(void *pvParameters)
         ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %" PRId64,
                  esp_http_client_get_status_code(client),
                  esp_http_client_get_content_length(client));
-        char buffer[1024] = {0};
-        int data_read = esp_http_client_read_response(client, buffer, sizeof(buffer));
-        if (data_read >= 0)
-        {
-            ESP_LOGI(TAG, "HTTP POST Response: %s", buffer);
-        }
-        else
-        {
-            ESP_LOGE(TAG, "Failed to read response");
-        }
     }
     else
     {
